0d1331482ff2b0881baba86340ed56553a904ac7
Brian Keng <brian.keng@rubikloud.com>
2017-03-18 17:01:36 -0400

    First version of script.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# Bradley-Terry-Model
-Small script to compute Bradley–Terry model and upload results to Google Sheets
+# Bradley-Terry Model Ranking Script
+
+A small script to compute Bradley–Terry model that:
+
+1. Pulls down pairwise match data from a Google spreadsheet.
+2. Computes the Bradley-Terry model with a simple regularization scheme (dummy games).
+3. Uploads the scaled scores back to the same Google spreadsheet in a new tab.
+
+I quickly wrote this up in an afternoon so that we could track our relative
+rankings in ping ping at the office.  I put it on a cron job and give everyone
+access to the sheet at work.
+
+# Notes
+
+ * You will need to setup an API key for gspread to access your Google sheet: http://gspread.readthedocs.io/en/latest/oauth2.html
+ * Make sure you share the target Google sheet with the email address associated with your credentials (usually a randomly generated one).
+ * The "Game Data" sheet should have 5 columns: `date`, `Player A`, `Player B`, `Wins A`, `Wins B`
+ * The "Ranking" sheet show the score for each player scaled between 0 and 1000.  It can be interpreted as probability of Player A beating Player B is (Score_A) / (Score_A + Score_B).
---
+++ update_model.py
@@ -0,0 +1,164 @@
+import argparse
+import gspread
+import logging
+import numpy as np
+import os
+import pandas as pd
+import time
+
+from datetime import datetime
+from oauth2client.service_account import ServiceAccountCredentials
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+DUMMY_PLAYER = 'DUMMY PLAYER'
+
+
+def connect_sheet(creds_file, sheet_key):
+    scope = ['https://spreadsheets.google.com/feeds']
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(creds_file, scope)
+    gc = gspread.authorize(credentials)
+
+    return gc.open_by_key(sheet_key)
+
+
+def extract_game_data(sheet, sheet_name):
+    worksheet = sheet.worksheet(sheet_name)
+    rawdata = worksheet.get_all_values()
+
+    df = pd.DataFrame(rawdata[1:], columns=rawdata[0])
+    assert all(c in df.columns for c in ['Date', 'Player A', 'Player B', 'Wins A', 'Wins B']), \
+        'Expecting columns Date, Player A, Player B, Wins A, Wins B'
+
+    df['Date'] = df['Date'].astype(datetime)
+    df['Wins A'] = df['Wins A'].astype(int)
+    df['Wins B'] = df['Wins B'].astype(int)
+
+    return df
+
+
+def add_dummy_games(game_data, alpha=1):
+    ''' Regularizes the estimate by adding games against a dummy player.
+
+        :param alpha: regularization parameter, number dummy wins/loses to add
+    '''
+    players = sorted(list(set(game_data['Player A']) | set(game_data['Player B'])))
+
+    # Add dummy games
+    dummy_data = [[datetime(2000, 1, 1), p, DUMMY_PLAYER, alpha, alpha] for p in players]
+    df = pd.DataFrame(dummy_data, columns=game_data.columns)
+    df = pd.concat([game_data, df])
+    df
+
+    return df
+
+
+def compute_rank_scores(game_data, max_iters=100, error_tol=1e-3):
+    ''' Computes Bradley-Terry using iterative algorithm
+
+        See: https://en.wikipedia.org/wiki/Bradley%E2%80%93Terry_model
+    '''
+    # Do some aggregations for convenience
+    # Total wins per player
+    winsA = game_data.groupby('Player A').agg(sum)['Wins A'].reset_index()
+    winsA = winsA[winsA['Wins A'] > 0]
+    winsA.columns = ['Player', 'Wins']
+    winsB = game_data.groupby('Player B').agg(sum)['Wins B'].reset_index()
+    winsB = winsB[winsB['Wins B'] > 0]
+    winsB.columns = ['Player', 'Wins']
+    wins = pd.concat([winsA, winsB]).groupby('Player').agg(sum)['Wins']
+
+    # Total games played between pairs
+    num_games = {}
+    for index, row in game_data.iterrows():
+        key = tuple(sorted([row['Player A'], row['Player B']]))
+        total = sum([row['Wins A'], row['Wins B']])
+        num_games[key] = total
+
+    # Iteratively update 'ranks' scores
+    players = sorted(list(set(game_data['Player A']) | set(game_data['Player B'])))
+    ranks = pd.Series(np.ones(len(players)), index=players)
+    for _ in range(max_iters):
+        oldranks = ranks.copy()
+        for player in ranks.index:
+            denom = np.sum(num_games.get(tuple(sorted([player, p])), 0.)
+                           / (ranks[p] + ranks[player])
+                           for p in ranks.index if p != player)
+            ranks[player] = 1.0 * wins[player] / denom
+
+        ranks /= sum(ranks)
+
+        if np.sum((ranks - oldranks).abs()) < error_tol:
+            break
+
+    del ranks[DUMMY_PLAYER]
+
+    # Scale score to be between 0 and 1000
+    ranks = (1000 * ranks.sort_values(ascending=False)).astype(int)
+
+    return ranks
+
+
+def upload_to_gsheets(sheet, ranks, rank_sheet):
+    ranking_ws = [w for w in sheet.worksheets() if w.title == rank_sheet]
+    if not ranking_ws:
+        worksheet = sheet.add_worksheet(title=rank_sheet, rows=100, cols=25)
+    else:
+        worksheet = ranking_ws[0]
+
+    worksheet.clear()
+    worksheet.update_cell(1, 1, 'Player')
+    worksheet.update_cell(1, 2, 'Score')
+
+    if worksheet.row_count < len(ranks) + 1:
+        worksheet.resize(rows=len(ranks) + 1, cols=25)
+
+    player_cells = worksheet.range(2, 1, len(ranks) + 1, 1)
+    for cell, player in zip(player_cells, ranks.index):
+        cell.value = player
+
+    value_cells = worksheet.range(2, 2, len(ranks) + 1, 2)
+    for cell, val in zip(value_cells, ranks.values):
+        cell.value = val
+
+    worksheet.update_cells(player_cells + value_cells)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Script to update Google Sheet with ranking model")
+    parser.add_argument('--creds-file', type=str, required=True,
+                        help='Google Drive API sheet')
+    parser.add_argument('--sheet-key', default=None, type=str, help='Google sheet key from URL')
+    parser.add_argument('--backup-dir', default=None, type=str,
+                        help='dir to backup game data')
+    parser.add_argument('--data-sheet', default='Game Data', type=str,
+                        help='Name of worksheet containing game data')
+    parser.add_argument('--rank-sheet', default='Ranking', type=str,
+                        help='Name of worksheet to write rankings')
+    parser.add_argument('--alpha', default=1, type=int, help='Regularization parameter')
+    args = parser.parse_args()
+
+    logging.info("Connecting to Google sheets '%s'...", args.sheet_key)
+    sheet = connect_sheet(args.creds_file, args.sheet_key)
+
+    logging.info("Extracting game data from '%s'...", args.data_sheet)
+    game_data = extract_game_data(sheet, args.data_sheet)
+    logging.info(" * Found %d rows.", len(game_data))
+
+    if args.backup_dir:
+        filename = "game_data-%d.csv.gz" % int(time.time())
+        logging.info(" * Saving backup to '%s'", filename)
+        game_data.to_csv(os.path.join(args.backup_dir, filename),
+                         compression='gzip', index=False)
+
+    logging.info("Adding dummy game for regularization (alpha=%d)...", args.alpha)
+    game_data = add_dummy_games(game_data, args.alpha)
+
+    logging.info("Computing rank scores...")
+    ranks = compute_rank_scores(game_data)
+
+    logging.info("Uploading rank scores to sheet '%s'...", args.rank_sheet)
+    ranks = upload_to_gsheets(sheet, ranks, args.rank_sheet)
+
+    logging.info("Done")
